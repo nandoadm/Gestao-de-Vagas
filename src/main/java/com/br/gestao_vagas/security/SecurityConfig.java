36161ba39b2e2733a3da1d223d80c10a5def2f38
admnando <nando.comth@gmail.com>
2025-03-25 16:47:19 -0300

    configuração de rotas

--- src/main/java/com/br/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/br/gestao_vagas/security/SecurityConfig.java
@@ -7,13 +7,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
-    //Bean - Utilizado para sobrescrever as configurações de um metodo
+    //Lista com rotas públicas
+    private final String[] authorities = {
+            "/candidate/",
+            //"/job/",
+            "/company/"
+    };
+
+
+    //Bean - Utilizado para sobrescrever as configurações de um método
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(auth ->{
+//            authorities -> lista com rotas públicas
+                auth.requestMatchers(authorities).permitAll();
+//      para qualquer  outra rota é necessario authenticação
+        auth.anyRequest().authenticated();});
 
         return http.build();
     };
